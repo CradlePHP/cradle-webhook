e2e27a678009c951f0c4724b9756ca215b930343
aprilsacil <aprilvsacil@gmail.com>
2018-05-30 20:43:55 +0800

    added events for sending the post data and for sending a subscription confirmation receipt

--- src/events.php
+++ src/events.php
@@ -3,6 +3,8 @@
  * This file is part of Cradle Webhook Package.
  */
 
+use Cradle\Curl\Rest;
+
 /**
  * Creates a webhook
  *
@@ -139,4 +141,88 @@ $this->on('webhook-update', function ($request, $response) {
     // 3. Process Data
     // trigger model update
     $this->trigger('system-model-update', $request, $response);
+});
+
+/**
+ * Performs webhook sending of data
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('webhook-send', function ($request, $response) {
+    //----------------------------//
+    // 1. Get Data
+    $data = [];
+    if($request->hasStage()) {
+        $data = $request->getStage();
+    }
+
+    //----------------------------//
+    // 2. Validate Data
+    $errors = [];
+    if (!isset($data['url'])
+        || empty($data['url'])
+        || filter_var($data['url'], FILTER_VALIDATE_URL)) {
+        $errors['url'] = 'Invalid url';
+    }
+
+    //----------------------------//
+    // 3. Prepare Data
+    $url = $data['url'];
+    unset($data['url']);
+
+    //----------------------------//
+    // 4. Process Data
+    Rest::i($url, true)
+        ->set($data)
+        ->setNotificationType('Even')
+        ->post();
+
+    $response->setError(false);
+});
+
+/**
+ * Performs sending of subscription notification
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('webhook-subscription', function ($request, $response) {
+    //----------------------------//
+    // 1. Get Data
+    $data = [];
+    if($request->hasStage()) {
+        $data = $request->getStage();
+    }
+
+    //----------------------------//
+    // 2. Validate Data
+    $errors = [];
+    if (!isset($data['url'])
+        || empty($data['url'])
+        || filter_var($data['url'], FILTER_VALIDATE_URL)
+    ) {
+        $errors['url'] = 'Invalid url';
+    }
+
+    //----------------------------//
+    // 3. Prepare Data
+    $url = $data['url'];
+    unset($data['url']);
+
+    $protocol = 'http';
+    if ($request->getServer('SERVER_PORT') === 443) {
+        $protocol = 'https';
+    }
+
+    $host = $protocol . '://' . $request->getServer('HTTP_HOST');
+
+    //----------------------------//
+    // 4. Process Data
+    Rest::i($url, true)
+        ->setSubscriptionUrl($host)
+        ->setNotificationType('SubscriptionConfirmation')
+        ->post();
+
+    $response->setError(false);
 });
